95003fd92ad5df0bb59221e814ed996c47da608c
Lê Nhật Minh <minecraftgamemcgame2@gmail.com>
2025-02-03 20:15:17 +0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function rickroll() {
-  window.open("https://youtu.be/p7YXXieghto", "_blank");
+  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
 }
 
 function loadtooltip() {
